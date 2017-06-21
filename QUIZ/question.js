b8bc544b26d3cf5b7098f362af569c3b9081f871
putrirrahma <putri2127ft2016@student.uny.ac.id>
2017-06-21 15:10:37 +0700

    Add files via upload

---
+++ QUIZ/question.js
@@ -0,0 +1,72 @@
+var questions =[{
+	"question" : "Hasil dari 8 + 4 = ",
+	"option1" : "7",
+	"option2" : "11",
+	"option3" : "12",
+	"option4" : "14",
+	"answer" : "3"
+	}, {
+	"question" : "Hasil dari 4 + 3 = ",
+	"option1" : "7",
+	"option2" : "10",
+	"option3" : "18",
+	"option4" : "6",
+	"answer" : "1"
+	}, {
+	"question" : "Hasil dari 12 : 4 = ",
+	"option1" : "3",
+	"option2" : "4",
+	"option3" : "2",
+	"option4" : "6",
+	"answer" : "1"	
+	}, {
+	"question" : "Hasil dari 17 + 3 = ",
+	"option1" : "15",
+	"option2" : "22",
+	"option3" : "20",
+	"option4" : "23",
+	"answer" : "3"
+	}, {
+	"question" : "Hasil dari 32 : 4 = ",
+	"option1" : "5",
+	"option2" : "6",
+	"option3" : "7",
+	"option4" : "8",
+	"answer" : "4"
+	}, {
+	"question" : "Sudut sebesar 35 derajat termasuk  = ",
+	"option1" : "Sudut pelanar",
+	"option2" : "Sudut tumpul",
+	"option3" : "Sudut lancip",
+	"option4" : "Sudut siku-siku",
+	"answer" : "3"
+	}, {
+	"question" : "Hasil dari 2 x 8 = ",
+	"option1" : "18",
+	"option2" : "15",
+	"option3" : "24",
+	"option4" : "16",
+	"answer" : "4"
+	}, {
+	"question" : "Hasil dari 32 + 29 = ",
+	"option1" : "61",
+	"option2" : "51",
+	"option3" : "53",
+	"option4" : "63",
+	"answer" : "1"
+	}, {
+	"question" : "Hasil dari 73 - 12 = ",
+	"option1" : "53",
+	"option2" : "44",
+	"option3" : "61",
+	"option4" : "51",
+	"answer" : "3"
+	}, {
+	"question" : "Hasil dari 5 x 8 = ",
+	"option1" : "40",
+	"option2" : "35",
+	"option3" : "45",
+	"option4" : "30",
+	"answer" : "1"
+	}
+];
